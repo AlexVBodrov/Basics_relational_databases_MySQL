cea9d0bb4e6290b77e474769f75f732d45ed65b4
AlexeiBodrov <77302302+AlexVBodrov@users.noreply.github.com>
2021-05-29 00:54:12 +0300

    добавлена ДЗ к уроку 2
    добавлена ДЗ к уроку 2. Вся ДЗ что по ссылке представлена в Word файле: lesson_2_BD.docx
     , ДЗ что была в презентации представлена в файле : Домашнее задание из презентации.sql

---
+++ dump.sql
@@ -0,0 +1,7 @@
+--с командной строки пишем
+mysqldump example > example.sql
+--заходим в mysql>
+CREATE DATABASE sample;
+--выходим из mysql> exit
+--с командной строки пишем
+mysql sample < example.sql
